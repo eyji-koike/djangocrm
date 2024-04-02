3a97ee21814293f7f1c47db58463a7784f9191c7
EyjiCuff <eyji.koike@gmail.com>
2024-04-01 23:13:57 -0300

    42 - making login

--- website/views.py
+++ website/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 # Create your views here.
+def home(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        print(username, password)
+
+    return render(request, 'home.html')
+
+
+def login_user(request):
+    pass
+
+
+def logout_user(request):
+    pass
