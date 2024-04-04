f323850ee9fdbefdf13d3a8086bc517137029d7b
EyjiCuff <eyji.koike@gmail.com>
2024-04-04 20:42:27 -0300

    Finished CRM with CRUD operations

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignupForm
+from .forms import SignupForm, AddRecordForm
 from .models import Record
 
 
@@ -52,3 +52,51 @@ def register_user(request):
         return render(request, 'register.html', {'form': form})
 
     return render(request, 'register.html', {'form': form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        costumer_record = Record.objects.get(pk=pk)
+        return render(request, 'records.html', {'costumer_record': costumer_record})
+    else:
+        messages.error(request, 'You have not been logged in!')
+        return redirect('home')
+
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, 'Record has been deleted!')
+        return redirect('home')
+    else:
+        messages.error(request, 'You have not been logged in!')
+        return redirect('home')
+
+
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated and request.method == "POST" and form.is_valid():
+        add_record = form.save()
+        messages.success(request, 'Record has been added!')
+        return redirect('home')
+    elif request.user.is_authenticated and request.method != "POST":
+        return render(request, 'add_record.html', {'form': form})
+    else:
+        messages.error(request, 'You have not been logged in!')
+        return redirect('home')
+
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Record has been updated!')
+            return redirect("home")
+        else:
+            return render(request, 'update_record.html', {'form': form})
+    else:
+        messages.error(request, 'You have not been logged in!')
+        return redirect('home')
